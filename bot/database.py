51d36794947a643f1ad5ec73eb5e0ad01a3b98f0
timursb <abramovtimur993@gmail.com>
2025-03-04 11:16:50 +0300

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -5,7 +5,7 @@ import random
 conn = sqlite3.connect("photos.db")
 cursor = conn.cursor()
 
-# Создаём таблицу
+# Создаём таблицу, если её нет
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS photos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,27 +16,19 @@ CREATE TABLE IF NOT EXISTS photos (
 """)
 conn.commit()
 
-
 def add_photo(file_id: str, user_id: int, album_name: str):
     """Добавляет фото в базу данных с привязкой к альбому"""
     cursor.execute("INSERT INTO photos (file_id, user_id, album_name) VALUES (?, ?, ?)", (file_id, user_id, album_name))
     conn.commit()
 
-
-def get_random_photo_from_album(user_id: int, album_name: str):
-    """Получает случайное фото из указанного альбома"""
-    cursor.execute("SELECT file_id FROM photos WHERE user_id = ? AND album_name = ?", (user_id, album_name))
+def get_random_photo_from_album(album_name: str):
+    """Получает случайное фото из указанного альбома (от любого пользователя)"""
+    cursor.execute("SELECT file_id FROM photos WHERE album_name = ?", (album_name,))
     photos = cursor.fetchall()
 
     if photos:
         return random.choice(photos)[0]  # Возвращаем случайное фото
     return None
-
-
-def delete_photo(file_id: str):
-    """Удаляет конкретное фото по file_id"""
-    cursor.execute("DELETE FROM photos WHERE file_id = ?", (file_id,))
-    conn.commit()
 
 def delete_album(album_name: str):
     """Удаляет все фото из указанного альбома"""
